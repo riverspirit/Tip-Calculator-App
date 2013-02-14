9e1943c938ed04775470b2374dee9fa20ec272b4
Sparky <sparky@veritas.(none)>
2013-02-14 17:23:51 +0530

    Basic design of the app

---
+++ js/calc.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	try {
+
+        $(function() {
+
+        });
+
+    } catch (error) {
+        console.error("Your javascript has an error: " + error);
+    }
+});
